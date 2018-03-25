8d0b133888c827c56bf1719f4ed3ed82712d5d16
Dongwoo Kim <dongwoo.kim@anu.edu.au>
2018-03-26 10:54:01 +1100

    minor update

--- data_utils/init_db.py
+++ data_utils/init_db.py
@@ -10,10 +10,9 @@ HOST = data_utils.REDIS_HOST
 PORT = data_utils.REDIS_PORT
 
 metadb = redis.Redis(host=HOST, db=METADB, port=PORT, decode_responses=True)
-metadb.flushall()
+# metadb.flushall()     # be careful this will remove the current databases
 
 metadb.set('db:name2id', NAME2ID)
 metadb.set('db:id2name', ID2NAME)
 metadb.set('db:wiki_ner', WIKI_NER)
-metadb.set('db:id2uid', WIKI_NER)
-
+metadb.set('stat:wiki_parsed', 0)
--- data_utils/wiki_extractor.py
+++ data_utils/wiki_extractor.py
@@ -3,7 +3,7 @@ from data_utils import *
 import itertools
 import logging
 import redis
-import json
+import uuid
 from nltk.tag.stanford import CoreNLPNERTagger
 import concurrent.futures as confu
 
@@ -36,9 +36,10 @@ def extract_ners(tokens, tagger, rdb):
 
     valid_ners = list()
     for ner in ners:
-        if lookup_fb(ner, rdb) is not None:
-            if ner not in valid_ners:
-                valid_ners.append(ner)
+        # ner = ((tokens), tag)
+        if lookup_fb(ner[0], rdb) is not None:
+            if ner[0] not in valid_ners:
+                valid_ners.append(ner[0])
 
     if len(valid_ners) >= 2:
         return merged_tokens, valid_ners
@@ -46,23 +47,27 @@ def extract_ners(tokens, tagger, rdb):
     return None
 
 
-def process_sentence(tokens, ners):
+def process_sentence(tokens, ners, sdb):
     """
     Find position (starting from 0) of NERs in the list of tokens.
     For all possible pairs in NERs, yield their positions in the token list
     :param tokens: list of tokens forming a sentence
     :param ners: list of ners in the token list
+    :param sdb: sentence database
     """
     ner_pos = datautils.get_nerspos(tokens, ners)
+
+    sid = uuid.uuid1()
+    sdb.set(sid, encode_rds(tokens))
 
     num_ner = len(ner_pos)
     for first_ner in range(num_ner - 1):
         for second_ner in range(1, num_ner):
             for pos1, pos2 in itertools.product(ner_pos[first_ner], ner_pos[second_ner]):
-                yield (ners[first_ner], ners[second_ner], pos1, pos2, tokens)
+                yield (ners[first_ner], ners[second_ner], pos1, pos2, sid)
 
 
-def update_db(rdb, en1, en2, en1pos, en2pos, tokens):
+def update_db(rdb, en1, en2, en1pos, en2pos, sid):
     """
     Update database to add a sentence along with the position of two entities in the sentence.
 
@@ -71,7 +76,7 @@ def update_db(rdb, en1, en2, en1pos, en2pos, tokens):
     :param en2: list, The second entity
     :param en1pos: The position of the first entity in the list of tokens
     :param en2pos: The position of the second entity in the list of tokens
-    :param tokens: list of tokens
+    :param sid: id of sentence in sentence database
     :return:
     """
     key = encode_rds((en1, en2))
@@ -79,7 +84,7 @@ def update_db(rdb, en1, en2, en1pos, en2pos, tokens):
 
     rdb.hset(key, (cnt, "en1pos"), en1pos)
     rdb.hset(key, (cnt, "en2pos"), en2pos)
-    rdb.hset(key, (cnt, "sentence"), encode_rds(tokens))
+    rdb.hset(key, (cnt, "sentence"), sid)
 
 
 if __name__ == '__main__':
@@ -93,6 +98,7 @@ if __name__ == '__main__':
     meta_db = redis.Redis(host=REDIS_HOST, db=0, port=REDIS_PORT, decode_responses=True)
     name2id_db = redis.Redis(host=REDIS_HOST, db=NAME2ID, port=REDIS_PORT, decode_responses=True)
     wiki_db = redis.Redis(host=REDIS_HOST, db=WIKI_NER, port=REDIS_PORT, decode_responses=True)
+    sentence_db = redis.Redis(host=REDIS_HOST, db=SENTENCE, port=REDIS_PORT, decode_responses=True)
 
     _skip = int(meta_db.get('stat:wiki_parsed'))
 
@@ -113,19 +119,20 @@ if __name__ == '__main__':
                             if result is not None:
                                 tokens = result[0]
                                 ners = result[1]
-                                logger.info('Sentence {}'.format(' '.join(tokens)))
+                                logger.info('Sentence {}'.format(tokens))
                                 logger.info('NERS {}'.format(ners))
-                                for en1, en2, en1pos, en2pos, tokens in process_sentence(tokens, ners):
-                                    update_db(wiki_db, en1, en2, en1pos, en2pos, tokens)
-
+                                for en1, en2, en1pos, en2pos, sid in process_sentence(tokens, ners, sentence_db):
+                                    update_db(wiki_db, en1, en2, en1pos, en2pos, sid)
 
                         except ValueError as e:
                             logger.debug(e)
 
                     token_list = list()
 
-                if i % 1000 == 0:
+                if i % 100 == 0:
                     logger.info('{} lines processed'.format(i))
+
+                if i < _skip:
                     meta_db.set('stat:wiki_parsed', i)
 
         except Exception as e:
