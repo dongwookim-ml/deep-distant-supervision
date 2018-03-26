1069068d037e7d0352b39cf12998729f1f5965f2
Dongwoo Kim <dongwoo.kim@anu.edu.au>
2018-03-26 13:33:33 +1100

    minor update

--- data_utils/init_db.py
+++ data_utils/init_db.py
@@ -5,6 +5,7 @@ METADB = data_utils.METADB
 NAME2ID = data_utils.NAME2ID
 ID2NAME = data_utils.METADB
 WIKI_NER = data_utils.WIKI_NER
+SENTENCE = data_utils.SENTENCE
 
 HOST = data_utils.REDIS_HOST
 PORT = data_utils.REDIS_PORT
@@ -16,3 +17,5 @@ metadb.set('db:name2id', NAME2ID)
 metadb.set('db:id2name', ID2NAME)
 metadb.set('db:wiki_ner', WIKI_NER)
 metadb.set('stat:wiki_parsed', 0)
+metadb.set('db:sentence', SENTENCE)
+
