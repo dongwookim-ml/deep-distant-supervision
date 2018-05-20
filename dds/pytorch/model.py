b65dabe6c845ade47993d3c1d40ef0d68e8ba0f1
Dongwoo Kim <dongwoo.kim@anu.edu.au>
2018-05-20 22:49:36 +1000

    implement word-level & sentence-level attention in pytorch.NRE

--- dds/pytorch/model.py
+++ dds/pytorch/model.py
@@ -12,20 +12,33 @@ ch = logging.StreamHandler(sys.stdout)
 ch.setFormatter(logging.Formatter('%(name)s:%(levelname)s:%(asctime)s:%(message)s'))
 logger = logging.getLogger()
 logger.addHandler(ch)
-logger.setLevel('DEBUG')
+logger.setLevel('INFO')
 
 
-class Attention(nn.Module):
+class SentenceAttention(nn.Module):
+    def __init__(self, input_dim):
+        super(SentenceAttention, self).__init__()
+        self.w1 = nn.Parameter(torch.randn(input_dim, requires_grad=True))
+
+    def forward(self, input):
+        # shape (batch, hidden_dim) * (hidden_dim, 1) -> (1, hidden_dim)
+        attn = torch.einsum('ik,k->i', [input, self.w1])
+        norm_attn = F.softmax(attn).clone()
+        summary = torch.einsum("ik,i->k", [input, norm_attn])
+        return summary
+
+
+class WordAttention(nn.Module):
     """
     Simple attention layer
     """
 
     def __init__(self, input_dim):
-        super(Attention, self).__init__()
+        super(WordAttention, self).__init__()
         self.w1 = nn.Parameter(torch.randn(input_dim, requires_grad=True))
 
     def forward(self, input):
-        # shape (batch, seq_len, hidden_dim) * (hidden_dim, 1) -> (batch, seq_len, 1)
+        # shape (batch, seq_len, hidden_dim) * (hidden_dim, 1) -> (batch, hidden_dim)
         attn = torch.einsum('ijk,k->ij', [input, self.w1])
         norm_attn = F.softmax(attn, 1).clone()
         summary = torch.einsum("ijk,ij->ik", [input, norm_attn])
@@ -45,24 +58,32 @@ class DDS(nn.Module):
         self.w2v = nn.Embedding(num_embeddings=num_voca, embedding_dim=embed_dim, _weight=embed)
         self.pos1vec = nn.Embedding(num_voca, pos_dim)
         self.pos2vec = nn.Embedding(num_voca, pos_dim)
-        input_dim = embed_dim + 2 * pos_dim
-        self.rnn = nn.RNN(input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True,
+        self.input_dim = embed_dim + 2 * pos_dim
+        self.rnn = nn.RNN(input_size=self.input_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True,
                           bidirectional=True)
-        self.attn = Attention(hidden_dim * 2)
+        self.word_attn = WordAttention(hidden_dim * 2)
+        self.sen_attn = SentenceAttention(hidden_dim * 2)
         self.linear = nn.Linear(hidden_dim * 2, num_relations, bias=True)
         logger.info('Done')
 
-    def forward(self, sen, pos1, pos2):
-        word_embedding = self.w2v(sen)
-        pos1_embedding = self.pos1vec(pos1)
-        pos2_embedding = self.pos2vec(pos2)
+    def forward(self, x):
+        seq_len = list()
+        max_seq = len(x[0][0])
+        batch_in = torch.zeros((len(x), max_seq, self.input_dim))
+        for i, (sen, pos1, pos2) in enumerate(x):
+            seq_len.append(len(sen))
+            _word = self.w2v(torch.from_numpy(sen))
+            _pos1 = self.pos1vec(torch.from_numpy(pos1))
+            _pos2 = self.pos2vec(torch.from_numpy(pos2))
+            combined = torch.cat((_word, _pos1, _pos2), 1)
+            batch_in[i, :len(sen)] = combined
 
-        combined = torch.cat((word_embedding, pos1_embedding, pos2_embedding), 1)
-        unsqueezed = combined.unsqueeze(0)
-
-        rnn_output, _ = self.rnn(unsqueezed)
-        sen_embedding = self.attn(rnn_output)
-        output = self.linear(sen_embedding)
+        batch_in = nn.utils.rnn.pack_padded_sequence(batch_in, seq_len, batch_first=True)
+        rnn_output, _ = self.rnn(batch_in)
+        unpacked, unpacked_len = torch.nn.utils.rnn.pad_packed_sequence(rnn_output)
+        sen_embeds = self.word_attn(unpacked)
+        pair_embeds = self.sen_attn(sen_embeds)
+        output = self.linear(pair_embeds)
         return output
 
 
@@ -91,20 +112,18 @@ if __name__ == '__main__':
     loss_fn = nn.BCEWithLogitsLoss()
 
     for i, (x, y) in enumerate(tqdm(fetcher, total=len(triple))):
-        unsqueezed_y = torch.from_numpy(y).float().unsqueeze(0)
-        for sentence in x:
-            sen, pos1, pos2 = sentence
-            sen = torch.from_numpy(sen)
-            pos1 = torch.from_numpy(pos1)
-            pos2 = torch.from_numpy(pos2)
-            # feed each sentence to model
-            output = model(sen, pos1, pos2)
+        x = sorted(x, key=lambda x: len(x[0]))
+        x.reverse()
+        print([len(k[0]) for k in x])
+        _y = torch.from_numpy(y).float()
 
-            loss = loss_fn(output, unsqueezed_y)
+        output = model(x)
+        loss = loss_fn(output, _y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        logger.debug('%d Done, loss = %f' % (i, loss))
 
         if i % 10000 == 0:
-            logger.info('%d entity pairs are processed')
+            logger.info('%d entity pairs are processed' % i)
